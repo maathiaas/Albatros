70f3238ebe49e3923be25a0c4194f60b68ddd572
matias <matiasgallardo.do2@gmail.com>
2019-05-13 20:24:24 -0400

    funcionan2

--- users/views.py
+++ users/views.py
@@ -26,14 +26,3 @@ def SignUp(request):
         form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
-#def SignUp(request):
-#    if request.method=='POST':
-#        form = CustomUserCreationForm(request.POST)
-#        if form.is_valid():
-#            form.save
-#            return redirect('/users')
-#    else:
-#        form = CustomUserCreationForm()
-
-#        args = {'form': form}
-#        return render(request, 'users/signup.html', args)
