b22b9b10b7472c4b545871453160572a9f54df56
matias <matiasgallardo.do2@gmail.com>
2019-05-21 20:35:41 -0400

    Union

--- myvenv/Scripts/Activate.ps1
+++ myvenv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\leoba\Documents\proyecto\Albatros\myvenv"
+$env:VIRTUAL_ENV="C:\Users\Matias\Documents\GitHub\Albatros\myvenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
